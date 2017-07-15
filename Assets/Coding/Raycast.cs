6097474f6137446b37c0ef2e0745d9677ce7ce4e
nizar333 <nizar333.na@gmail.com>
2017-07-15 12:33:57 +0700

    new
    new

---
+++ Assets/Coding/Raycast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	void Start () {
+
+		mainCamera = FindObjectOfType<Camera> ();
+		
+	}
+	
+
+	void Update () {
+
+		Ray cameraRay = mainCamera.ScreenPointToRay (Input.mousePosition);
+		Plane GroundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayLength;
+
+		if (GroundPlane.Raycast (cameraRay, out rayLength)) {
+
+			Vector3 pointToLook = cameraRay.GetPoint (rayLength);
+			Debug.DrawLine (cameraRay.origin, pointToLook, Color.green);
+
+			transform.LookAt (new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		}
+	}
+}
